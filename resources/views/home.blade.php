9a7c233449ff703ce51616e3d7cfdc7ef8d7dce9
ameerharram <ameer.harram@maslowcreative.com>
2021-09-06 01:03:15 +0500

    add popup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,25 +10,25 @@
       <p>Choose one of the quick options for easier navigation</p>
 
       <div class="grid options-grid">
-        <div class="bg-white option-item">
-          <a href="#">
-            <ion-icon name="person-add-outline" class="option-item-icon"></ion-icon> Add New User
-          </a>
-        </div>
-        <div class="bg-white option-item">
-          <a href="#">
-            <span class="admin-icon">
-              <ion-icon name="person-add-outline" class="option-item-icon"></ion-icon>
-              <ion-icon name="cog-outline" class="icon-cog"></ion-icon>
-            </span> Add New Admin
-          </a>
-        </div>
-        <div class="bg-white option-item"><a href="#">
-            <ion-icon name="push-outline" class="option-item-icon"></ion-icon> Bulk Upload
-          </a></div>
-        <div class="bg-white option-item"><a href="#">
-            <ion-icon name="push-outline" class="flip-v option-item-icon"></ion-icon>Bulk Download
-          </a></div>
+        <a href="#" class="option-item">
+          <ion-icon name="person-add-outline" class="option-item-icon"></ion-icon>
+          Add New User
+        </a>
+        <a href="#" class="option-item" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <span class="admin-icon">
+            <ion-icon name="person-add-outline" class="option-item-icon"></ion-icon>
+            <ion-icon name="cog-outline" class="icon-cog"></ion-icon>
+          </span>
+          Add New Admin
+        </a>
+        <a href="#" class="option-item">
+          <ion-icon name="push-outline" class="option-item-icon"></ion-icon>
+          Bulk Upload
+        </a>
+        <a href="#" class="option-item">
+          <ion-icon name="push-outline" class="flip-v option-item-icon"></ion-icon>
+          Bulk Download
+        </a>
       </div>
     </div>
 
@@ -41,9 +41,17 @@
                 <h5>Invoices/Claims</h5>
                 <small class="text-primary">9999 Invoices/Claims</small>
               </div>
-              <div>
+              <div class="card-right-btns">
                 <button class="btn btn-primary">All Claims</button>
-                <button class="btn btn-light">Pending Approval</button>
+                <div class="dropdown">
+                  <button class="btn btn-light" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                    aria-expanded="false">Pending Approval</button>
+                  <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </div>
+                </div>
                 <button class="btn btn-light btn-icon">
                   <ion-icon name="funnel-outline"></ion-icon>
                 </button>
@@ -215,7 +223,7 @@
                 <h5>Users</h5>
                 <small class="text-primary">9999 Active Users</small>
               </div>
-              <div>
+              <div class="card-right-btns">
                 <button class="btn btn-primary btn-icon">
                   <ion-icon name="add-outline"></ion-icon> Add New User
                 </button>
@@ -266,7 +274,7 @@
                 <h5>Sub-Admins</h5>
                 <small class="text-primary">99999 Sub-admins</small>
               </div>
-              <div>
+              <div class="card-right-btns">
                 <button class="btn btn-primary btn-icon">
                   <ion-icon name="add-outline"></ion-icon> Sub-Admins
                 </button>
@@ -279,7 +287,174 @@
         </div>
       </div>
     </div>
-
   </div>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" data-bs-backdrop="static" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+      <div class="modal-content addUserPopup">
+        <div class="modal-header">
+          <h4 class="modal-title" id="exampleModalLabel">Create a new user</h4>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body addUser">
+          <div class="row">
+            <div class="col-md-6">
+              <ul class="stepList">
+                <li>
+                  <a class="title d-inline-flex align-items-center" href="#">
+                    <div class="step-number">1</div>
+                    <div class="step-text">
+                      <h6>Choose User Type</h6>Provider, Participant or Representative
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a class="title d-inline-flex align-items-center" href="#">
+                    <div class="step-number">2</div>
+                    <div class="step-text">
+                      <h6>Personal Information</h6>Enter your user’s personal information
+                    </div>
+                  </a>
+
+                </li>
+                <li>
+                  <a class="title d-inline-flex align-items-center" href="#">
+                    <div class="step-number">3</div>
+                    <div class="step-text">
+                      <h6>Contact Information</h6>Enter User’s Contact information
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a class="title d-inline-flex align-items-center" href="#">
+                    <div class="step-number">4</div>
+                    <div class="step-text">
+                      <h6>NDIS Information</h6>Provide information related to NDIS
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a class="title d-inline-flex align-items-center" href="#">
+                    <div class="step-number">5</div>
+                    <div class="step-text">
+                      <h6>Login Information</h6>Set-up a password for your new user
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              {{-- Step 1 --}}
+              <div class="step1">
+                <h6>Choose your User Type</h6>
+                <div class="d-grid gap-2">
+                  <div>
+                    <input type="checkbox" class="btn-check" id="btn-check-1" autocomplete="off">
+                    <label class="btn btn-light d-block" for="btn-check-1">Provider</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" class="btn-check" id="btn-check-2" autocomplete="off">
+                    <label class="btn btn-light d-block" for="btn-check-2">Participant</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" class="btn-check" id="btn-check-3" autocomplete="off">
+                    <label class="btn btn-light d-block" for="btn-check-3">Representative</label>
+                  </div>
+                </div>
+              </div>
+              {{-- Step 2 --}}
+              <div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="mb-3">
+                      <label for="fullName" class="form-label">Full Name</label>
+                      <input type="text" class="form-control" id="fullName" placeholder="The Name of the Provider">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="mb-3">
+                      <label for="fullName2" class="form-label">Full Name</label>
+                      <input type="text" class="form-control" id="fullName2" placeholder="The Name of the Provider">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="mb-3">
+                      <label for="linkAParticipant" class="form-label">Link a Participant</label>
+                      <input type="text" class="form-control" id="linkAParticipant" placeholder="Participant’s Name">
+                    </div>
+                    <div>
+                      <div class="bg-light d-flex align-items-center p-3">
+                        <div><img src="/images/avatar.png" width="40" alt=""></div>
+                        <div class="flex-1">
+                          <h6>Users Name </h6>
+                          <span class="text-primary">Participant</span>
+                        </div>
+                        <div class="ms-auto">
+                          <button class="btn btn-link p-0">
+                            <ion-icon name="remove-circle-outline"></ion-icon>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {{-- Step 3 --}}
+              <div>
+                <div>
+                  <div class="mb-3">
+                    <label for="homeAddress" class="form-label">Home Address</label>
+                    <textarea name="" id="" cols="30" rows="2" class="form-control" id="homeAddress" placeholder="Home Address 13, 
+            10000 State, Country."></textarea>
+                  </div>
+                  <div class="mb-3">
+                    <label for="emailAddress" class="form-label">E-mail Address</label>
+                    <input type="text" class="form-control" id="emailAddress" placeholder="providersemail@gmail.com">
+                  </div>
+                  <div class="mb-3">
+                    <label for="phoneNumber" class="form-label">Phone Number</label>
+                    <input type="tel" class="form-control" id="phoneNumber" placeholder="0000000000">
+                  </div>
+                </div>
+              </div>
+              {{-- Step 4 --}}
+              <div>
+                <div>
+                  <div class="mb-3">
+                    <label for="abnNumber" class="form-label">ABN</label>
+                    <input type="tel" class="form-control" id="abnNumber" placeholder="00000000000000000000">
+                  </div>
+                  <div class="mb-3">
+                    <label for="specificItemNumbers" class="form-label">Specific Item Numbers</label>
+                    <textarea name="" id="" cols="30" rows="2" class="form-control" id="specificItemNumbers"
+                      placeholder="000000000000, 000000
+            0000000000, 0000000"></textarea>
+                  </div>
+                </div>
+              </div>
+              {{-- Step 5 --}}
+              <div>
+                <div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="emailRequest">
+                    <label class="form-check-label" for="emailRequest">
+                      Send email request for the new user to create password
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        {{-- <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div> --}}
+      </div>
+    </div>
+  </div>
+
 </main>
 @endsection
