1414942be1ff77cbe967a6d47651419e04dfe6cf
ameerharram <ameer.harram@maslowcreative.com>
2021-09-27 23:13:28 +0500

    changes

--- resources/js/components/popups/CreateAdminPopup.vue
+++ resources/js/components/popups/CreateAdminPopup.vue
@@ -1,27 +1,4 @@
 <template>
-  <div
-    class="modal fade"
-    id="adminModal"
-    tabindex="-1"
-    data-bs-backdrop="static"
-    aria-labelledby="adminModalTitle"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog modal-lg modal-dialog-centered">
-      <div class="modal-content addUserPopup">
-        <div class="modal-header">
-          <h4 class="modal-title" id="adminModalTitle">Create a new Sub-admin</h4>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <form @submit.prevent="createAdmin" method="POST">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="mb-4">
-                  <label for="fullName" class="form-label">Full Name</label>
-                  <input type="text" v-model="form.name" class="form-control" id="fullName" placeholder="Admin Name" />
-                  <div class="invalid-msg" v-if="form.errors.has('name')" v-html="form.errors.get('name')" />
-                </div>
     <div class="modal fade" id="adminModal" tabindex="-1" data-bs-backdrop="static" aria-labelledby="adminModalTitle"
          aria-hidden="true">
         <div class="modal-dialog modal-lg modal-dialog-centered">
@@ -99,26 +76,26 @@
                             </div>
                             <div class="col-md-12">
                                 <div class="d-flex justify-content-between align-items-center mb-4">
+                                    <label class="fw-bold">Export / Import Documents</label>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="editOption5" checked>
+                                        <label class="form-check-label" for="editOption5"></label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mw290 mx-auto px-4 mt-3">
+                                <button v-if="!loader" class="btn btn-primary btn-lg w-100 py-3">Create Sub-admin</button>
+                                <button v-else class="btn btn-primary btn-lg w-100 py-3" type="button" disabled>
+                                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                    Loading...
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-              </div>
-              <div class="col-md-12">
-                <div class="d-flex justify-content-between align-items-center mb-4">
-                  <label class="fw-bold">Export / Import Documents</label>
-                  <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="editOption5" checked />
-                    <label class="form-check-label" for="editOption5"></label>
-                  </div>
-                </div>
-              </div>
-              <div class="mw290 mx-auto px-4 mt-3">
-                <button class="btn btn-primary btn-lg w-100 py-3">Create Sub-admin</button>
-              </div>
             </div>
-          </form>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -149,11 +126,13 @@ export default {
                 .post(route)
                 .then(res => {
                     if ((res.status = 201)) {
-                        
                         this.resetForm();
+                        this.$toastr.s("Success","Account created!");
                     }
                 })
-                .catch(error => {})
+                .catch(error => {
+                    this.$toastr.e("Error","Some thing went wrong.");
+                })
                 .finally(()=>{
                     this.loader = false;
                 })
