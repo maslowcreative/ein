2b08f396f44c8cfe2341b8ef7ac05129e1277ca1
ameerharram <ameer.harram@maslowcreative.com>
2021-10-13 00:28:25 +0500

    service controller

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class Service extends Model
 {
     use HasFactory;
     protected $primaryKey = "item_number";
+
+    /**
+     * Relationships
+     */
+    public static function getServices()
+    {
+        return QueryBuilder::for(Service::class)
+                ->allowedFilters([
+                    AllowedFilter::partial('item_number','item_number')
+                ]);
+    }
 }
