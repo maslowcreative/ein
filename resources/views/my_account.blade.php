5d9fb15d27b4127526c17566ac98494746ab35a9
ameerharram <ameer.harram@maslowcreative.com>
2021-10-17 18:10:21 +0500

    my account.

--- resources/views/my_account.blade.php
+++ resources/views/my_account.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="main">
-        <div class="container-xxl">
-            
+<main class="main">
+  <div class="container-xxl">
+    <div class="card">
+      <div class="card-body">
+        <h3 class="border-bottom pb-4 mb-5">My Account</h3>
+
+        <div class="row">
+          
         </div>
-    </main>
+      </div>
+    </div>
+  </div>
+</main>
 @endsection
