e6a17e500fbde3e26fd269e26adb7fac14485591
Ameer Harram <ameerharram@gmail.com>
2022-08-28 13:48:36 +0500

    cat page

--- resources/js/components/common/analytics/SpendingsGraph.vue
+++ resources/js/components/common/analytics/SpendingsGraph.vue
@@ -2,7 +2,18 @@
     <div class="card">
         <div class="card-body">
             <div>
-                <canvas id="myChart"></canvas>
+                <Bar
+                    :chart-options="chartOptions"
+                    :chart-data="chartData"
+                    :chart-id="chartId"
+                    :dataset-id-key="datasetIdKey"
+                    :plugins="plugins"
+                    :css-classes="cssClasses"
+                    :styles="styles"
+                    :width="width"
+                    :height="height"
+                />
+
             </div>
         </div>
     </div>
@@ -10,52 +21,99 @@
 
 <script>
 
+import { Bar } from 'vue-chartjs/legacy'
 
-import {Chart} from "chart.js/auto";
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
-    components: {},
-    data()  {
-        return {
-            labels : [
-                'January',
-                'February',
-                'March',
-                'April',
-                'May',
-                'June',
-            ],
-            data : {
-                labels: this.labels,
-                datasets: [{
-                    label: 'My First dataset',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 5, 2, 20, 30, 45],
-                }]
-            },
-            config : {
-                type: 'line',
-                data: this.data,
-                options: {}
-            },
-            canvas : null,
+    name: 'BarChart',
+    components: {
+        Bar
+    },
+    props: {
+        chartId: {
+            type: String,
+            default: 'bar-chart'
+        },
+        datasetIdKey: {
+            type: String,
+            default: 'label'
+        },
+        width: {
+            type: Number,
+            default: 400
+        },
+        height: {
+            type: Number,
+            default: 400
+        },
+        cssClasses: {
+            default: '',
+            type: String
+        },
+        styles: {
+            type: Object,
+            default: () => {}
+        },
+        plugins: {
+            type: Array,
+            default: () => []
         }
     },
-    mounted() {
-        var ctx = document.getElementById('myChart');
-        this.canvas = new Chart(ctx, {
-            type: 'line',
-            data: this.data,
-            options: {}
-        });
-        // console.log('chuchu',this.config);
-        // this.canvas = new Chart(ctx, this.config);
-        //this.canvas.update();
-    },
+    data() {
+        return {
+            chartData: {
+                labels: [
+                   'Assistance with Daily Life',
+                   'Assistance with Social, Economic and Community Participation',
+                   'Assistive Technology',
+                   'Consumables',
+                   'Finding and Keeping a Job',
+                   'Home Modifications and Specialised Disability Accommodation (SDA)',
+                   'Improved Daily Living Skills',
+                   'Improved Health and Wellbeing',
+                   'Improved Learning',
+                   'Improved Life Choices',
+                   'Improved Living Arrangements',
+                   'Improved Relationships',
+                   'Increased Social and Community Participation',
+                   'Support Coordination',
+                   'Transport'
+                ],
+                datasets: [
+                    {
+                        label: 'Spent',
+                        backgroundColor: '#36a2eb',
+                        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11, 13 ,14, 15]
+                    },
+                    {
+                        label: 'Pending',
+                        backgroundColor: '#ff6484',
+                        data: [5,6,7,8,9,10,15,14,13,12,11,1,2,3,4,5]
+                    },
+                    {
+                        label: 'Balance',
+                        backgroundColor: '#f87979',
+                        data: [1,2,3,4,5,6,7,8,9,10,12,13,14,15]
+                    }
+                ]
 
-    methods: {
-
+            },
+            chartOptions: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
     }
 }
 </script>
