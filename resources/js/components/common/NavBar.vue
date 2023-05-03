5359cf1ee7340186dda8231d0dd4a7d4b9b3ea04
Ameer Harram <ameerharram@gmail.com>
2023-05-03 10:04:52 +0500

    Test

--- resources/js/components/common/NavBar.vue
+++ resources/js/components/common/NavBar.vue
@@ -21,7 +21,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
           <li class="nav-item">
-            <a class="nav-link"  :href="baseUrl">Dashboard</a>
+            <a class="nav-link"  :href="baseUrl">Dashboard!</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" :href="myAccountUrl">My Account</a>
