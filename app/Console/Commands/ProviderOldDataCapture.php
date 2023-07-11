d86e94301b8ea7571fe5825b0469e614cc6e5bdf
Ameer Harram <ameerharram@gmail.com>
2023-07-11 11:39:57 +0500

    script change

--- app/Console/Commands/ProviderOldDataCapture.php
+++ app/Console/Commands/ProviderOldDataCapture.php
@@ -44,23 +44,25 @@ class ProviderOldDataCapture extends Command
     public function handle()
     {
         $plans = Plan::get()->toArray();
+
         config(['database.connections.mysql.strict' => false]);
         DB::reconnect();
 
-        DB::beginTransaction();
-
+        //DB::beginTransaction();
+        ProviderBudget::truncate();
         foreach ($plans as $plan){
 
             $lineItems = DB::table('claim_line_items')->where('plan_id',$plan['id'])->whereIn('status',[
                                 //Claim::STATUS_APPROVAL_PENDING,
                                 Claim::STATUS_APPROVED_BY_REPRESENTATIVE,
                                 Claim::STATUS_APPROVED_BY_ADMIN,
-                                Claim::STATUS_PROCESSED,
-                                Claim::STATUS_RECONCILATION_DONE
+                                Claim::STATUS_PROCESSED
                             ])
                             ->select('plan_id','category_id','provider_id','participant_id',DB::raw("IFNULL( sum(amount_claimed),0) as amount_claimed, IFNULL( sum(amount_paid),0) as amount_paid, status"))
                             ->groupBy('plan_id','provider_id','category_id','participant_id')
                             ->get();
+
+
             if($lineItems->count()> 0){
 
                 foreach ($lineItems as $item)
@@ -69,9 +71,9 @@ class ProviderOldDataCapture extends Command
                     $planBudget = PlanBudget::where('plan_id',$item->plan_id)->where('category_id',$item->category_id)->first();
                     if($planBudget){
 
+                        $pending = $item->amount_claimed;
+                        $spent = 0;
                         $amount = $item->amount_claimed;
-                        $pending = $item->amount_claimed - $item->amount_paid;
-                        $spent = $item->amount_paid;
 
                         $providerBudget = ProviderBudget::where('provider_id',$item->provider_id)
                                                           ->where('category_id',$item->category_id)
@@ -82,13 +84,67 @@ class ProviderOldDataCapture extends Command
                         if(!$providerBudget){
                             $providerBudget = new ProviderBudget();
                             $providerBudget->amount = $amount;
+                            $providerBudget->pending = $pending;
+                            $providerBudget->spent = $spent;
                         }else
                         {
-                            $providerBudget->amount = $providerBudget->amount;
+                            $providerBudget->amount = $providerBudget->amount + $amount;
+                            $providerBudget->pending = $providerBudget->pending + $pending;
+                            $providerBudget->spent = $providerBudget->spent + $spent;
                         }
-                        $providerBudget->pending = $pending;
-                        $providerBudget->spent = $spent;
-                        $providerBudget->balance = $providerBudget->amount - ($pending + $spent);
+
+                        $providerBudget->balance = 0;
+
+                        $providerBudget->provider_id = $item->provider_id;
+                        $providerBudget->plan_id = $item->plan_id;
+                        $providerBudget->plan_budget_id = $planBudget->id;
+                        $providerBudget->category_id = $item->category_id;
+                        $providerBudget->save();
+                    }
+                }
+            }
+
+
+
+            $recLineItems = DB::table('claim_line_items')->where('plan_id',$plan['id'])->whereIn('status',[
+                    Claim::STATUS_RECONCILATION_DONE
+                ])
+                ->select('plan_id','category_id','provider_id','participant_id',DB::raw("IFNULL( sum(amount_claimed),0) as amount_claimed, IFNULL( sum(amount_paid),0) as amount_paid, status"))
+                ->groupBy('plan_id','provider_id','category_id','participant_id')
+                ->get();
+
+
+            if($recLineItems->count()> 0){
+
+                foreach ($lineItems as $item)
+                {
+
+                    $planBudget = PlanBudget::where('plan_id',$item->plan_id)->where('category_id',$item->category_id)->first();
+                    if($planBudget){
+
+                        $pending = 0;
+                        $spent = $item->amount_paid;
+                        $amount = $item->amount_paid;
+
+                        $providerBudget = ProviderBudget::where('provider_id',$item->provider_id)
+                            ->where('category_id',$item->category_id)
+                            ->where('plan_budget_id',$planBudget->id)
+                            ->where('plan_id',$item->plan_id)
+                            ->first();
+
+                        if(!$providerBudget){
+                            $providerBudget = new ProviderBudget();
+                            $providerBudget->amount = $amount;
+                            $providerBudget->pending = $pending;
+                            $providerBudget->spent = $spent;
+                        }else
+                        {
+                            $providerBudget->amount = $providerBudget->amount + $amount;
+                            $providerBudget->pending = $providerBudget->pending + $pending;
+                            $providerBudget->spent = $providerBudget->spent + $spent;
+                        }
+
+                        $providerBudget->balance = 0;
 
                         $providerBudget->provider_id = $item->provider_id;
                         $providerBudget->plan_id = $item->plan_id;
@@ -100,7 +156,7 @@ class ProviderOldDataCapture extends Command
             }
 
         }
-        DB::commit();
+        //DB::commit();
         config(['database.connections.mysql.strict' => true]);
         DB::reconnect();
         return 0;
